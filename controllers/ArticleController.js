07032e1e64a8161ab8deb2453ca119f256cee856
William Silva <williamsilva808@hotmail.com>
2016-10-10 23:31:09 -0300

    Update package.json and initials implements article controller

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -6,4 +6,24 @@ module.exports = {
 		res.json({text: 'List posts CMS' });
 	},
 
+	findArticleById(req,res){
+		res.json({text: 'Find post by id CMS' });
+	},
+
+	findArticleByTitle(req,res){
+		res.json({text: 'Find post by title CMS' });
+	},
+
+	cerate(req,res){
+		res.json({text: 'create post CMS' });
+	},
+
+	update(req,res){
+		res.json({text: 'update post CMS' });
+	},
+
+	remove(req,res){
+		res.json({text: 'remove post CMS' });
+	},
+
 };
